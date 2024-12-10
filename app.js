dcdd5b9f8fa73a622eeea318d567f2655b386831
Iuri Ciciliotti <iurimattosc@gmail.com>
2024-12-10 13:26:57 -0300

    projeto pessoal

---
+++ app.js
@@ -0,0 +1,23 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do desafio';
+
+const consoleButton = document.querySelector('.button');
+
+consoleButton.addEventListener('click', () => {
+  if (consoleButton.textContent.trim() === 'Console') {
+    console.log('Você clicou no botão Console!');
+  }
+});
+function exibirAlerta() {
+  alert('Eu amo O Nilson')
+};
+function perguntaNome() {
+  let nomeDaCidade = prompt('Digite o nome de uma cidade do Brasil que você gosta muito')
+  alert(`Estive em ${nomeDaCidade} e lembrei de você`)
+};
+function somaDeNumeros() {
+  let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
+  let segundoNumero = parseInt(prompt('Digite o segundo número'));
+  let resultado = primeiroNumero + segundoNumero;
+  alert(`${primeiroNumero} + ${segundoNumero} = ${resultado}`)
+}
