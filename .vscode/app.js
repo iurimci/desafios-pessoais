5101e94c7a92cb593e6c613c184464c85a4aee00
Iuri Ciciliotti <iurimattosc@gmail.com>
2024-12-11 14:44:37 -0300

    TentantoSegunda

--- .vscode/app.js
+++ .vscode/app.js
@@ -18,6 +18,7 @@ function perguntaNome() {
 function somaDeNumeros() {
   let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
   let segundoNumero = parseInt(prompt('Digite o segundo número'));
-  let resultado = primeiroNumero + segundoNumero ;
-  alert(`${primeiroNumero} + ${segundoNumero} = ${resultado}`)
+  let terceiroNumero = parseInt(prompt('Digite o segundo número'));
+  let resultado = primeiroNumero + segundoNumero + terceiroNumero ;
+  alert(`${primeiroNumero} + ${segundoNumero} + ${terceiroNumero} = ${resultado}`)
 }
