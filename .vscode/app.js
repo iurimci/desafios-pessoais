1d7489f92aab5327d6b6ef78ea6d7561523cd0fa
Iuri Ciciliotti <iurimattosc@gmail.com>
2024-12-11 14:37:16 -0300

    Diminuir soma

--- .vscode/app.js
+++ .vscode/app.js
@@ -18,7 +18,6 @@ function perguntaNome() {
 function somaDeNumeros() {
   let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
   let segundoNumero = parseInt(prompt('Digite o segundo número'));
-  let terceiroNumero = parseInt(prompt('Digite o terceiro número'));
   let resultado = primeiroNumero + segundoNumero + terceiroNumero;
-  alert(`${primeiroNumero} + ${segundoNumero} + ${terceiroNumero} = ${resultado}`)
+  alert(`${primeiroNumero} + ${segundoNumero} + = ${resultado}`)
 }
